d0847cd7b138c03b1d94eaf9660e2fe816373150
eunbeen kang <145640625+river20s@users.noreply.github.com>
2026-01-28 20:10:07 +0900

    [Bronze V] Title: A/B, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,15 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // Scanner sc = new Scanner(System.in);
         
         // float: 유효숫자 6~7자리(오차수준 10^(-7)),
         // double: 유효숫자 15~16자리(오차수준 10^(-16))
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
         
+        // double a = sc.nextDouble();
+        // double b = sc.nextDouble();
+        
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
         System.out.println(a / b);
     
     }
