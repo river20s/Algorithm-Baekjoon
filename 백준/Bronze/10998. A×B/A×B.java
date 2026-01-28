c9d375b0ae8ab81457a1a56b5e514f33ee3bbd90
eunbeen kang <145640625+river20s@users.noreply.github.com>
2026-01-28 19:54:03 +0900

    [Bronze V] Title: A×B, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -2,10 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a * b);
+        
+        /*
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println(a * b);
+        */
     }
 }
