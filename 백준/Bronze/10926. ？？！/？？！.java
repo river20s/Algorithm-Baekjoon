8bf43a846ed42c56986c29956c1f1334d788b9ee
eunbeen kang <145640625+river20s@users.noreply.github.com>
2026-01-28 20:33:10 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,12 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-   
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String overlappingID = br.readLine();     
-        System.out.println(overlappingID + "??!");
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String id = br.readLine();
+        
+        // Scanner sc = new Scanner(System.in);
+        // String id = sc.next();
+        System.out.println(id + "??!");
+        
     }
 }
