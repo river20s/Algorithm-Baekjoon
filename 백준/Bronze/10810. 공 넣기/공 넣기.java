0b25d882b6918fa2010f54620d114ee3532f3551
eunbeen kang <145640625+river20s@users.noreply.github.com>
2026-01-29 20:31:30 +0900

    [Bronze III] Title: 공 넣기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,35 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
+/* 바구니
+[1] [2] [1] [1] [0]
+ 1   2   3   4   5
+배열을 갱신하는?
+*/
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] input = br.readLine().split(" ");
-
-        int n = Integer.parseInt(input[0]);  // 바구니의 개수
-        int m = Integer.parseInt(input[1]); // 공 넣을 방법 개수
-
-        int[] baskets = new int[n]; // 바구니 배열
-
-        for (int i = 0; i < m; i++) { // m번 입력 받음
-            String[] temps = br.readLine().split(" ");
-            int startNumber = Integer.parseInt(temps[0]) - 1; // 공 넣기 시작할 바구니 배열 인덱스
-            int endNumber = Integer.parseInt(temps[1]) - 1; // 공 마지막으로 넣을 바구니 배열 인덱스
-            int ballNumber = Integer.parseInt(temps[2]); // 넣을 공의 번호
-
-            for (int j = startNumber; j <= endNumber; j++) { // 바구니 배열에 공 넣기
-                baskets[j] = ballNumber;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] b = new int[n]; // 바구니
+        
+        /* 선언시 자동으로 0으로 초기화됨
+        for (i = 0; i < n; i++) {
+            // 일단 다 0으로 초기화
+            b[i] = 0;
+        }
+        */
+        
+        for (int q = 0; q < m; q++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()); // 시작 바구니 번호
+            int j = Integer.parseInt(st.nextToken()); // 끝 바구니 번호
+            int k = Integer.parseInt(st.nextToken()); // 넣을 공
+            
+            for (int idx = i - 1; idx <= j - 1; idx++) {
+                b[idx] = k;
             }
         }
         
-        // 바구니 배열 요소 출력 (공백으로 구분)
-        for (int i = 0; i < n; i++) {
-            System.out.print(baskets[i]);
-            if (i < n - 1) { // 마지막 요소가 아닌 경우에만 공백 추가
-                System.out.print(" ");
-            }
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int q = 0; q < n; q++) {
+            sb.append(b[q]);
+            if (q < n - 1) sb.append(" ");
         }
+        System.out.println(sb);
     }
 }
