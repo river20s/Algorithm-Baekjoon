1d3de2dbab97ac45a0e7ef6901cee5b8eb3c59b9
eunbeen kang <145640625+river20s@users.noreply.github.com>
2026-02-26 16:59:29 +0900

    [level 2] Title: 올바른 괄호, Time: 6.85 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = True
+    
+    count = 0
+    
+    for c in s:
+        if c == '(':
+            count += 1
+        else:
+            count -= 1
+            
+        if count < 0:
+            return False
+        
+        
+    answer = count == 0
+
+
+    return answer
